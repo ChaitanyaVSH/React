2ac8293cd09113b254f84862f115ed6c7d5d8ad6
Chaitanya <chaitu.vsh@gmail.com>
2020-10-02 17:14:24 +0530

    FreeCodeCamp: Implemented a navbar FC with jumbotrons

---
+++ src/components/first.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const BioData = () => {
+  return (
+    <div className="jumbotron jumbotron-fluid bg-success text-white">
+      <div className="container">
+        <h1 className="display-1">Hey, Good Morning Chaitu Maverick</h1>
+        <p>Affirm the good and the bad will vanish...!</p>
+      </div>
+    </div>
+  );
+};
+
+export default BioData;

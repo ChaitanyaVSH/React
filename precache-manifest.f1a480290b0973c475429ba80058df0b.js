68021953312a388fda29593758de450f972abbb3
Chaitanya <chaitu.vsh@gmail.com>
2020-10-02 23:32:14 +0530

    Updates

---
+++ precache-manifest.f1a480290b0973c475429ba80058df0b.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08643da745b60967b7ad2fb09d75ae17",
+    "url": "/React/index.html"
+  },
+  {
+    "revision": "82787d1e124e49f4ddc5",
+    "url": "/React/static/css/2.d1848c0e.chunk.css"
+  },
+  {
+    "revision": "7ed7e0a10cd9f00cb54d",
+    "url": "/React/static/css/main.645983f5.chunk.css"
+  },
+  {
+    "revision": "82787d1e124e49f4ddc5",
+    "url": "/React/static/js/2.838fd461.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React/static/js/2.838fd461.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ed7e0a10cd9f00cb54d",
+    "url": "/React/static/js/main.e54dbe07.chunk.js"
+  },
+  {
+    "revision": "4fa4d27e6a993539c681",
+    "url": "/React/static/js/runtime-main.626dd583.js"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/React/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/React/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/React/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/React/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/React/static/media/glyphicons-halflings-regular.fa277232.woff"
+  }
+]);

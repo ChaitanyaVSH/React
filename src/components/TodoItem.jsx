3a617b4447a7f7ebcfbef6384131e808254febd3
Chaitanya <chaitu.vsh@gmail.com>
2020-10-05 23:06:15 +0530

    Learned ReactJS: Life Cycle methods and their best usecases

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -20,6 +20,50 @@ class TodoItem extends Component {
     this.props.finished(this.props.todo);
   };
 
+  /**
+   * LifeCycle Methods.
+   * 1. These methods are run only once through the webpage loaded.
+   * 2. These methods are useful to make any AJAX calls to the end-points or Backend systems to make the data available even before the render method is called to desing the Component.
+   */
+  componentDidMount() {
+    /**
+     * Perfect place to make an AJAX call.
+     */
+  }
+
+  componentWillReceiveProps(nextProps) {
+    /**
+     * This is run everytime the parent component sends the props
+     */
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    /**
+     * This life-cycle method is very useful to optimize the react app.
+     * There might be a scenario where the component is re-rendered without any change in the state or props, just because the parent component has rendered. This might add latency to the system.
+     *
+     * This method should return true or false based on the changes in the props/state.
+     */
+  }
+
+  componentWillUnmount() {
+    /**
+     * A clean up to be done can be put here
+     */
+  }
+
+  static getDerivedStateFromProps(props, state) {
+    /**
+     * This method can be used in a use-case where you want to update/create the state based on the incoming props.
+     */
+  }
+
+  getSnapshotBeforeUpdate() {
+    /**
+     * Create a backup of the data currently
+     */
+  }
+
   render() {
     // const styles = {
     //   This const will not create any TS issues.
